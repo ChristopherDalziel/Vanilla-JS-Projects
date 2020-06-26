9604dde5fe08be102d898807782bf277071b3867
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-06-26 20:53:46 +1000

    Data from app now saves to localStorage, issue where price of movie is displaying as NaN in bottom<p>

---
+++ movieSeating/script.js
@@ -0,0 +1,46 @@
+// Single selector
+const container = document.querySelector(".container");
+// Multiple selected => Puts all of the selected into a node list similar to an array which we can run array methods on
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+// Adding the '+' symbol infront of movieSelect makes it an integer instead of a string
+let ticketPrice = +movieSelect.value;
+
+setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+};
+
+updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeats * ticketPrice;
+};
+
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
