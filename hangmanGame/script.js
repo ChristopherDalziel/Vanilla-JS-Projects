822b6eb186e03bcb680224f0bd4e9b88efc45f76
Chris Dalziel <christopher_dalziel@hotmail.com>
2020-07-31 21:15:56 +1000

    added logic for displayed word and checks if the user has won the game

---
+++ hangmanGame/script.js
@@ -0,0 +1,38 @@
+const wordElement = document.getElementById("word");
+const wrongLettersElement = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-again");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = ["programming", "application", "javascript", "dog"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+displayWord = () => {
+  wordElement.innerHTML = `
+  ${selectedWord
+    .split("")
+    .map(
+      (letter) =>
+        `<span class="letter">${
+          correctLetters.includes(letter) ? letter : ""
+        }</span>`
+    )
+    .join("")} 
+  `;
+
+  const innerWord = wordElement.innerText.replace(/\n/g, "");
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations, You Won!";
+    popup.style.display = "flex";
+  }
+};
+
+displayWord();
